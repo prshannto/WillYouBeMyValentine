83d13830f9105de082200cce482c75bbc654565f
Prashant <prashanttt001@gmail.com>
2026-02-03 23:28:44 +0530

    first commit to project - prank

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center text-2xl gap-10 font-bold h-screen">
+      App
+      <button className="bg-pink-400 font-bold text-xl px-10 py-5 rounded-lg cursor-pointer">
+        No
+      </button>
+    </div>
+  );
+};
+
+export default App;
